27928103ff24fdd86861d1f0e53cd990ef918986
Nithish Reddy <nithishreddyb2@gmail.com>
2025-05-26 12:45:58 +0530

    Changed Stack.java

--- src/_8Stack/Stack.java
+++ src/_8Stack/Stack.java
@@ -1,78 +1,50 @@
 package _8Stack;
-
-public class Stack<T>{
-    private java.util.ArrayList<T> elements;
+import java.util.*;
+public class Stack<T> {
+    ArrayList<T> elements;
 
     public Stack(){
-        elements = new java.util.ArrayList<>();
+        elements = new ArrayList<>();
     }
-
-    public void push(T item){
-        elements.add(item);
+    public void push(T element){
+        elements.add(element);
+        System.out.println("element pushed into stack");
     }
-
-    public T pop(){
-        if(isEmpty()){
-            System.out.println("Stack underflow");
-            return null;
+    public void pop(){
+        if (elements.isEmpty()){
+            System.out.println("stack is empty");
+            return;
         }
-        return elements.remove(elements.size()-1);
+        System.out.println("element popped from the stack" + elements.removeLast());
     }
-
-    public void clear(){
-        elements.clear();
-    }
-
-    public boolean isEmpty(){
-        return elements.isEmpty();
-    }
-
     public void display(){
-        if(isEmpty()){
-            System.out.println("Stack is Empty");
-        }else{
-            System.out.println("Stack contents are: ");
-            for(int i = elements.size()-1 ; i >= 0 ; i--){
-                System.out.println(elements.get(i));
-            }
+        if(elements.isEmpty()){
+            System.out.println("stack is empty");
+            return;
+        }
+        System.out.println("stack elements:");
+        for (int i = elements.size() - 1 ; i >= 0 ; i--){
+            System.out.println(elements.get(i));
         }
     }
 }
 
 class Demo{
     public static void main(String[] args) {
-        Stack<Integer> integerStack = new Stack<>();
-        integerStack.push(1);
-        integerStack.push(2);
-        integerStack.push(3);
-        integerStack.push(4);
+        Stack<Integer> intStack = new Stack<>();
+        intStack.push(1);
+        intStack.push(2);
+        intStack.display();
+        intStack.pop();
+        intStack.display();
 
-        System.out.println("Integer Stack:");
-        integerStack.display();
+        Stack<String> strStack = new Stack<>();
 
-        integerStack.pop();
-        System.out.println("after popping one element: ");
-        integerStack.display();
-
-        integerStack.clear();
-        System.out.println("After clearing empty stack: ");
-        integerStack.display();
-
-
-        Stack<String> stringStack = new Stack<>();
-        stringStack.push("Hello");
-        stringStack.push("World");
-        stringStack.push("Java");
-        stringStack.push("Python");
-        stringStack.display();
-        System.out.println("Popping");
-        stringStack.pop();
-        stringStack.pop();
-        stringStack.display();
-        System.out.println("After clearing stack: ");
-        stringStack.clear();
-        stringStack.display();
-        System.out.println("----End of program----");
+        strStack.push("Hello");
+        strStack.push("World");
+        strStack.display();
+        strStack.pop();
+        strStack.display();
 
     }
 }
