b7d026010a6e45d46b9a93e0e65dd0b0cb9dbfa2
Nithish Reddy <nithishreddyb2@gmail.com>
2025-06-25 22:41:03 +0530

    updated file
    used HashMap

--- src/_7Book/Test.java
+++ src/_7Book/Test.java
@@ -6,31 +6,28 @@ public class Test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        List<Book> bookList = new ArrayList<>();
-        bookList.add(new Book(10,"Java","Nithish","PHI",1435));
-        bookList.add(new Book(11,"Java","Rahul","PHI",1245));
-        bookList.add(new Book(12,"Python","Nova","RA",14335));
+        HashMap<Integer,Book> bookMap = new HashMap<>();
+        bookMap.put(1,new Book(10,"Java","Nithish","PHI",1435));
+        bookMap.put(2,new Book(11,"Java","Rahul","PHI",1245));
+        bookMap.put(3,new Book(12,"Python","Nova","RA",14335));
 
         System.out.println("List of All Books: ");
-        for (Book bk : bookList){
+        for (Book bk : bookMap.values()){           //diff way to print
             System.out.println(bk);
         }
         System.out.println("---------------");
 
-        List<Book> sortedByPrice = new ArrayList<>(bookList);
-        sortedByPrice.sort(Comparator.comparingDouble(b -> b.price));
-//        for (Book bk : sortedByPrice){
-//            System.out.println(bk);
-//        }
-        Iterator<Book> it = sortedByPrice.iterator();
-        while(it.hasNext()){
-            System.out.println(it.next());
+        List<Book> sortedByPrice = new ArrayList<>(bookMap.values());
+        sortedByPrice.sort(Comparator.comparingDouble(b->b.price));
+        System.out.println("List of All Books SortedBy Price: ");
+        for (Book bk : sortedByPrice){
+            System.out.println(bk);
         }
         System.out.println("--------------");
 
         System.out.println("Enter an author's name to search: ");
         String searchAuthor = sc.nextLine();
-        for (Book bk : bookList){
+        for (Book bk : bookMap.values()){
             if(bk.author.equalsIgnoreCase(searchAuthor)){
                 System.out.println(bk);
             }
