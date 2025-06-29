e6271c41b591540627ade0472b5ab4c0ac462167
Nithish Reddy <nithishreddyb2@gmail.com>
2025-06-29 11:21:21 +0530

    Updated file
    Deleted Test.java and integrated the code

--- src/_7Book/Book.java
+++ src/_7Book/Book.java
@@ -1,13 +1,11 @@
 package _7Book;
-
 import java.util.*;
-public class Book {
-    public int id;
-    public String title,author,publisher;
-    public double price;
 
-    public Book(int id, String title, String author, String publisher, double price) {
-        this.id = id;
+public class Book {
+    String title, author, publisher;
+    double price;
+
+    Book(String title, String author, String publisher, double price) {
         this.title = title;
         this.author = author;
         this.publisher = publisher;
@@ -17,11 +15,51 @@ public class Book {
     @Override
     public String toString() {
         return "Book{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
+                "title='" + title + '\'' +
                 ", author='" + author + '\'' +
                 ", publisher='" + publisher + '\'' +
                 ", price=" + price +
                 '}';
+    }
+
+    static void sortBooksByPrice(Map<Integer, Book> bookMap) {
+        List<Book> books = new ArrayList<>(bookMap.values());
+        books.sort(Comparator.comparingDouble(b -> b.price));
+        System.out.println("Books sorted by price:");
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Map<Integer, Book> bookMap = new HashMap<>();
+        bookMap.put(1, new Book("Programming Java", "Joshua Bloch", "Pub", 1500));
+        bookMap.put(2, new Book("Programming C", "Robert C. Martin", "Pub", 1700));
+        bookMap.put(3, new Book("Programming Python", "Allen B. Downey", "Pub", 950));
+        sortBooksByPrice(bookMap);
+
+        System.out.print("\nEnter the author name to search: ");
+        String searchAuthor = sc.nextLine();
+
+        System.out.println("Books by " + searchAuthor + ":");
+        for (Book book : bookMap.values()) {
+            if (book.author.equalsIgnoreCase(searchAuthor)) {
+                System.out.println(book);
+            }
+        }
+
+        System.out.print("\nEnter the price threshold: ");
+        double priceThreshold = sc.nextDouble();
+
+        System.out.println("Books with price >= " + priceThreshold + ":");
+        for (Book book : bookMap.values()) {
+            if (book.price >= priceThreshold) {
+                System.out.println(book);
+            }
+        }
+
+        sc.close();
     }
 }
